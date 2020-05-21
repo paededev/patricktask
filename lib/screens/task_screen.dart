32f98b98fc4ce77fcbbc1c57ea4c7eb85b31a7dd
Patrick May <may.patrick@hotmail.com>
2020-05-21 22:56:21 +0200

    update taskscreens

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -6,28 +7,52 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.lightBlueAccent,
-        body: Container(
-          padding:
-              EdgeInsets.only(top: 100.0, left: 30.0, right: 30.0, bottom: 30.0),
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  child: Icon(
-                      Icons.list,
-                      size: 30.0,
-                  color: Colors.lightBlueAccent),
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
-                ),
-                SizedBox(height: 10.0),
-                Text('Patrick To Do',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w400),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding:
+                  EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Icon(
+                          Icons.list,
+                          size: 30.0,
+                      color: Colors.lightBlueAccent),
+                      backgroundColor: Colors.white,
+                      radius: 30.0,
+                    ),
+                    SizedBox(height: 10.0),
+                    Text('Patrick To Do',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w400),
+                    ),
+                    Text
+                      ('12 Tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                    ),
+
+                  ],
                 )
-              ],
-            )
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    )),
+              ),
+            ),
+          ],
         )
     );
   }
